5d47a1b7533bcefa74daea2a7e101c22ce5a394a
miltonbanksy <miltonbanksy@gmail.com>
2026-01-14 15:12:36 +0900

    refactor job fetching logic in script.js to filter and display illiterate jobs

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 fetch('medieval_jobs.json')
-    .then(response => response.json()
+    .then(response => response.json())
     .then(data => {
-        /*
-        // Print List on screen
-        const container = document.getElementById('data-container');
-        data.forEach(item => {
-            const div = document.createElement('div');
-            div.innerHTML = `Title: ${item.title}`;
-            container.appendChild(div);
-        });
-        */
         const selectMedievalJobs = document.getElementById('select-medieval-jobs');
-        data.forEach(item => {
-            const option = document.createElement('option');
-            option.textContent = `${item.title}`;
-            selectMedievalJobs.appendChild(option);
-        });
+        data
+            .filter(job => job.literacy === "illiterate")
+            .map(job => {
+                const option = document.createElement('option');
+                option.value = job.literacy;
+                option.textContent = job.title;
+                return option;
+        })
+        .forEach(option => selectMedievalJobs.appendChild(option));
     })
     .catch(error => {
         console.log('Error fetching the JSON file:', error);
-    })
-)
+});
